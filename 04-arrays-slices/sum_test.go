d1a72d4e3d07275d4f4e5d9647a6edd34d860912
Masaki Abe <abe.m@edocode.co.jp>
2023-09-08 14:37:08 +0900

    04: Change the Sum() func to work with a slice
    instead of an array

--- 04-arrays-slices/sum_test.go
+++ 04-arrays-slices/sum_test.go
@@ -3,12 +3,18 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
 
-	got := Sum(numbers)
-	want := 15
+		assertCorrectResult(t, got, want, numbers)
+	})
+}
 
+func assertCorrectResult(t testing.TB, got, want int, numbers []int) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %d want %d, given %v", got, want, numbers)
 	}
