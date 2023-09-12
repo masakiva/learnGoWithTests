1678d778fd0e48c2e44b68edb1ee84d89aa9f282
Masaki Abe <abe.m@edocode.co.jp>
2023-09-12 16:59:18 +0900

    04: Change to "table driven tests" + Add Triangle

--- 05-structs-methods-interfaces/shapes.go
+++ 05-structs-methods-interfaces/shapes.go
@@ -7,8 +7,8 @@ type Shape interface {
 }
 
 type Rectangle struct {
-	Width float64
-	Height float64
+	Width	float64
+	Height	float64
 }
 
 func (r Rectangle) Area() float64 {
@@ -16,17 +16,22 @@ func (r Rectangle) Area() float64 {
 }
 
 type Circle struct {
-	Radius float64
+	Radius	float64
 }
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+type Triangle struct {
+	Base	float64
+	Height	float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
